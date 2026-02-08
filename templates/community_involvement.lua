688f16d3bd9a8169801d7971430e7c90b01e7775
Eliza Brock Marcum <eliza@elizamarcum.com>
2026-02-07 18:08:28 -0800

    Second draft of two-column community involvement

--- templates/community_involvement.lua
+++ templates/community_involvement.lua
@@ -1,46 +1,33 @@
 function community_involvement_setup()
-  positionNames = {}
-  positionDates = {}
-  organizationNames = {}
-  positionDescriptions = {}
+  entries = {}
 end
 
 function record_community_involvement(positionName, positionDate, organizationName, positionDescription)
-  table.insert(positionNames, positionName)
-  table.insert(positionDates, positionDate)
-  table.insert(organizationNames, organizationName)
-  table.insert(positionDescriptions, positionDescription)
+  entry = {positionName=positionName, positionDate=positionDate, organizationName=organizationName, positionDescription=positionDescription}
+  table.insert(entries, entry)
 end
 
-function make_multiline_date(date_str)
-  if not string.find(date_str, "\\\\") then
-    date_str = date_str.."\\\\ \\\\"
+
+function output_community_involvement(entry)
+  if entry then
+    tex.sprint("\\rendercvcommunityinvolvemententry{"..entry.positionName.."}{"..entry.positionDate.."}{"..entry.organizationName.."}{"..entry.positionDescription.."}")
+  else
+    tex.sprint("Community Involvement Entry null?!")
   end
-  return "\\multirow{2}{*}{\\entrydatestyle{\\makecell[r]{"..date_str.."}}}"
 end
 
-function output_community_involvement()
+function output_community_involvements()
   tex.sprint("\\setlength\\tabcolsep{0pt}")
   tex.sprint("\\setlength{\\extrarowheight}{0pt}")
-  tex.sprint("\\begin{tabular}[t]{@{\\extracolsep{\\fill}} L{0.5\\textwidth - 2.6cm} R{2.5cm} @{\\hspace{0.2cm}} L{0.5\\textwidth - 2.6cm} R{2.5cm}}")
-  while next(positionNames) do
-    tex.sprint("\\entrytitlestyle{"..table.remove(positionNames, 1).."} \z
-              & "..make_multiline_date(table.remove(positionDates, 1)).." \z
-              & \\entrytitlestyle{"..table.remove(positionNames, 1).."} \z
-              & "..make_multiline_date(table.remove(positionDates, 1)).." \z
-              \\\\")
+  tex.sprint("\\begin{tabular}[t]{@{\\extracolsep{\\fill}} L{0.5\\textwidth-0.1cm} @{\\hspace{0.2cm}} L{0.5\\textwidth-0.1cm}}")
 
-    tex.sprint("\\entryorganizationstyle{"..table.remove(organizationNames, 1).."} \z
-              & \z
-              & \\entryorganizationstyle{"..table.remove(organizationNames, 1).."} \z
-              & \z
-              \\\\ ")
-
-    tex.sprint(" \\multicolumn{2}{L{0.5\\textwidth}}{\\entryorganizationnotestyle{"..table.remove(positionDescriptions, 1).."}} \z
-               & \\multicolumn{2}{L{0.5\\textwidth}}{\\entryorganizationnotestyle{"..table.remove(positionDescriptions, 1).."}} \z
-              \\\\ ")
-
-    tex.sprint(" & & & \\\\")
+  while next(entries) do
+    local entry = table.remove(entries, 1)
+    output_community_involvement(entry)
+    tex.sprint(" & ")
+    output_community_involvement(table.remove(entries, 1))
+    tex.sprint("\\\\")
   end
+
   tex.sprint("\\end{tabular}")
 end
